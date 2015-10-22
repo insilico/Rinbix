676b27373edf9e5549bcb2d84be7551868765c98
Bill White <bill.c.white@gmail.com>
2015-10-21 19:26:57 -0500

    WIP3: package name source file, data files.

---
+++ R/Rinbix.R
@@ -0,0 +1,18 @@
+#' Rinbix: In silico research group's bioinformatics toolbox for network and 
+#' machine learning analysis in bioinformatics. 1/30/14
+#'
+#' The Rinbix package provides several categories of important functions:
+#' * an R interface to the C++ library through system() calls
+#' * clustering
+#' * feature selection/ranking
+#' * classification
+#' * association networks
+#' * linear/logistic regression
+#' * data simulation
+#' * network analysis and visualization
+#' * utilities: statistical functions, filters
+#'
+#' @docType package
+#' @name Rinbix
+NULL
+#> NULL
